5389a88736a264dc133e889d7e9b6a9f875ad45d
Moolight_Z <1327706523@qq.com>
2020-08-04 23:34:59 +0800

    commit

--- src/main/java/com/moonlight/algorithm/assist/helper/AbstractHelper.java
+++ src/main/java/com/moonlight/algorithm/assist/helper/AbstractHelper.java
@@ -1,6 +1,11 @@
 package com.moonlight.algorithm.assist.helper;
 
+import java.lang.reflect.Field;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @ClassName AbstractHelper
@@ -21,5 +26,55 @@ public abstract class AbstractHelper {
      * @version
      **/
     public abstract Object create(ResultSet rs);
+
+    public <T> T create(ResultSet rs, Class<T> t){
+        T result = null;
+        Field field = null;
+        try {
+            Field[] declaredFields = t.getDeclaredFields();
+
+            int[] column2Property = column2Property(rs, declaredFields);
+            if (rs.next()) {
+                result = t.newInstance();
+                for (int i = 0, length = column2Property.length; i < length; i++) {
+                    if (column2Property[i] != -1) {
+                        field = declaredFields[column2Property[i]];
+                        field.setAccessible(true);
+                        Class<?> type = field.getType();
+                        if (type == Integer.class || type == Integer.TYPE) {
+                            field.set(result, rs.getInt(i));
+                        } else if (type == Byte.class || type == Byte.TYPE) {
+                            field.set(result, rs.getByte(i));
+                        } else if (type.equals(Date.class)) {
+                            field.set(result, rs.getDate(i));
+                        } else if (type == String.class) {
+                            field.set(result, rs.getString(i));
+                        }
+                    }
+                }
+            }
+        } catch (SQLException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private int[] column2Property(ResultSet rs, Field[] fields) throws SQLException {
+        int[] columnPropertyMapping = new int[fields.length];
+        Arrays.fill(columnPropertyMapping, -1);
+
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        for (int i = 1; i < columnCount + 1; i++) {
+            for (int j = 0, length = fields.length; j < length; j++) {
+                if (fields[j].getName().equalsIgnoreCase(metaData.getColumnName(i))) {
+                    columnPropertyMapping[i] = j;
+                    break;
+                }
+            }
+        }
+        return columnPropertyMapping;
+    }
 
 }
